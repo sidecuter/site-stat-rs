4164b6ee0c32eba23c33a6e7d80464434d850345
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 00:20:53 +0300

    Add Responder implementation for Status schema

--- src/schemas/status.rs
+++ src/schemas/status.rs
@@ -1,3 +1,5 @@
+use actix_web::body::BoxBody;
+use actix_web::Responder;
 use serde::Serialize;
 use utoipa::ToSchema;
 
@@ -8,6 +10,14 @@ pub struct Status {
 
 impl Default for Status {
     fn default() -> Self {
-        return Self{status: "OK".to_string()}
+        Self{status: "OK".to_string()}
+    }
+}
+
+impl Responder for Status {
+    type Body = BoxBody;
+
+    fn respond_to(self, _: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        actix_web::HttpResponse::Ok().json(self)
     }
 }
