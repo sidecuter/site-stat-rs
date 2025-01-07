fee41b69f2bcf1aaf5fe8bfaeb1e5e40df329296
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 01:54:09 +0300

    Remove useless crates

--- src/errors.rs
+++ src/errors.rs
@@ -1,24 +1,24 @@
 use actix_web::{error, http::{StatusCode, header::ContentType}, HttpResponse};
-use derive_more::derive::{Display, Error};
 use log::{log, Level};
 use sea_orm::DbErr;
 use crate::schemas::status;
 
-#[derive(Debug, Error, Display)]
+#[derive(Debug, thiserror::Error, Clone)]
 pub enum Error {
-    #[display("{message}")]
-    InternalError { message: String },
-    BadClientData,
-    #[display("{res} not found")]
-    NotFound { res: String },
+    #[error("{0}")]
+    InternalError(String),
+    #[error("{0}")]
+    UnprocessableData(String),
+    #[error("{0} not found")]
+    NotFound(String),
 }
 
 impl error::ResponseError for Error {
     fn status_code(&self) -> StatusCode {
         match *self {
-            Error::InternalError { .. } => StatusCode::INTERNAL_SERVER_ERROR,
-            Error::BadClientData => StatusCode::BAD_REQUEST,
-            Error::NotFound { .. } => StatusCode::NOT_FOUND,
+            Error::InternalError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::UnprocessableData(_) => StatusCode::UNPROCESSABLE_ENTITY,
+            Error::NotFound(_) => StatusCode::NOT_FOUND,
         }
     }
 
@@ -34,9 +34,9 @@ impl From<DbErr> for Error {
         log!(Level::Error, "{:?}", value.sql_err());
         let message = value.to_string();
         if message.contains("FOREIGN KEY") {
-            Self::NotFound {res: "External id".to_string()}
+            Self::NotFound("External id".to_string())
         } else {
-            Self::InternalError {message: value.to_string()}
+            Self::InternalError(value.to_string())
         }
     }
 }
