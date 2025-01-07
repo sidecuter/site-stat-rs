67754651e2cd0b0d1dfacfd854e6754c338c14a3
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 00:21:28 +0300

    Add endpoint to router

--- src/api/stat/mod.rs
+++ src/api/stat/mod.rs
@@ -1,7 +1,11 @@
+pub mod site;
+
 use actix_web::web;
+use crate::api::stat::site::stat_site;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/stat")
+            .service(stat_site)
     );
 }
