697530f0b73fb0bbf0d9fcff96cff320a7dd30fd
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 06:23:28 +0300

    Add conversion Trait

---
+++ src/traits/convert.rs
@@ -0,0 +1,31 @@
+use actix_web::Responder;
+use sea_orm::{DbErr, ModelTrait};
+use crate::errors::Result as ApiResult;
+use crate::schemas::status::Status;
+
+pub trait ConversionTrait<T> {
+    type Output;
+
+    fn convert(self) -> Self::Output;
+}
+
+pub trait ConversionToStatusTrait<T> {
+    type Output;
+
+    fn status_ok(self) -> Self::Output;
+}
+
+impl<T: Responder + From<W>, W: ModelTrait> ConversionTrait<T> for Result<W, DbErr> {
+    type Output = ApiResult<T>;
+
+    fn convert(self) -> Self::Output {
+        self.map_err(|e| e.into()).map(|v| v.into())
+    }
+}
+
+impl<T: Responder + From<Status>, W: ModelTrait> ConversionToStatusTrait<T> for Result<W, DbErr> {
+    type Output = ApiResult<T>;
+    fn status_ok(self) -> Self::Output {
+        self.map_err(|e| e.into()).map(|_| Status::default().into())
+    }
+}
