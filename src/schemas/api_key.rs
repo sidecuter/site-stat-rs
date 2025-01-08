ece436ccfb281f5532a04de6ea35bcdf9852f8a1
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 19:47:24 +0300

    Add type for api_key validation

---
+++ src/schemas/api_key.rs
@@ -0,0 +1,39 @@
+use std::fmt::{Display, Formatter};
+use serde::{Deserialize, Deserializer, Serialize};
+use regex::Regex;
+use utoipa::ToSchema;
+
+#[derive(Debug, Serialize, Clone, ToSchema)]
+#[schema(example = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef", pattern = r"^[0-9a-f]{64}$")]
+pub struct ApiKey(String);
+
+impl ApiKey {
+    const REGEX: &'static str = r"^[0-9a-f]{64}$";
+
+    fn validate(&self) -> Result<(), String> {
+        let re = Regex::new(Self::REGEX).unwrap();
+        re.is_match(&self.0).then(|| ()).ok_or(format!("Invalid api_key format: {}", self.0))
+    }
+
+    pub fn new(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl<'de> Deserialize<'de> for ApiKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: String = Deserialize::deserialize(deserializer)?;
+        let auditory_id = ApiKey(s);
+        auditory_id.validate().map_err(serde::de::Error::custom)?;
+        Ok(auditory_id)
+    }
+}
+
+impl Display for ApiKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
