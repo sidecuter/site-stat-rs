781dca1ff000bd715d6e39904096fec6d8bc41ae
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 22:29:49 +0300

    Reexport trait

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,7 +1,9 @@
 mod convert;
 mod filter;
 mod create;
+mod paginate;
 
 pub use self::filter::FilterTrait;
-pub use self::convert::{ConversionToStatusTrait, ConversionTrait};
+pub use self::convert::{ConversionToStatusTrait, ConversionTrait, ConversionToPaginationTrait};
 pub use self::create::CreateFromScheme;
+pub use self::paginate::Paginate;
