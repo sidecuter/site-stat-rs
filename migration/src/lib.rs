0cfdc9b65d08c8a66c88b1b5b7fde8bb6d18a3ba
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 16:03:02 +0300

    Add migration for auds population

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,11 +1,11 @@
 pub use sea_orm_migration::prelude::*;
 
 
-mod data;
+pub mod data;
 mod m20220101_000001_create_table;
 mod m20250107_204426_create_site_stat_table;
 mod m20250108_120158_create_auds_table;
-// mod m20250108_120630_populate_auds_table;
+mod m20250108_120630_populate_auds_table;
 
 pub struct Migrator;
 
@@ -16,7 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20250107_204426_create_site_stat_table::Migration),
             Box::new(m20250108_120158_create_auds_table::Migration),
-            // Box::new(m20250108_120630_populate_auds_table::Migration),
+            Box::new(m20250108_120630_populate_auds_table::Migration),
         ]
     }
 }
---
+++ migration/src/m20250108_120630_populate_auds_table.rs
@@ -0,0 +1,24 @@
+use sea_orm_migration::prelude::*;
+use sea_orm_migration::sea_orm::EntityTrait;
+use crate::data::AUDS;
+use entity::{auds::{ActiveModel}, prelude::Auds};
+use crate::sea_orm::ActiveValue::Set;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        let auds: Vec<_> = AUDS.iter().map(|aud| ActiveModel {id: Set(aud.to_string())}).collect();
+        // Replace the sample below with your own migration scripts
+        Auds::insert_many(auds).exec(db).await.map(|_| ())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        let db = manager.get_connection();
+        Auds::delete_many().exec(db).await.map(|_| ())
+    }
+}
