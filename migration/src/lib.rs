d78c7788ee1f624dcdff91bec2a40cf38c7d36c0
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 15:30:00 +0300

    Create migration for Auds table creation

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,7 +1,11 @@
 pub use sea_orm_migration::prelude::*;
 
+
+mod data;
 mod m20220101_000001_create_table;
 mod m20250107_204426_create_site_stat_table;
+mod m20250108_120158_create_auds_table;
+// mod m20250108_120630_populate_auds_table;
 
 pub struct Migrator;
 
@@ -11,6 +15,8 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20250107_204426_create_site_stat_table::Migration),
+            Box::new(m20250108_120158_create_auds_table::Migration),
+            // Box::new(m20250108_120630_populate_auds_table::Migration),
         ]
     }
 }
---
+++ migration/src/m20250108_120158_create_auds_table.rs
@@ -0,0 +1,40 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Auds::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Auds::Id)
+                            .string()
+                            .not_null()
+                            .primary_key()
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        manager
+            .drop_table(Table::drop().table(Auds::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+pub enum Auds {
+    Table,
+    Id,
+}
