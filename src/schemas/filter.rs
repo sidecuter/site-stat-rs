2aa56a2a323403e819fdd4ecd7ffddb180378cd9
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 22:53:01 +0300

    Add new filter

--- src/schemas/filter.rs
+++ src/schemas/filter.rs
@@ -1,8 +1,9 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sea_orm::{DatabaseConnection, DbErr, EntityTrait, ModelTrait, PaginatorTrait, QueryOrder};
 use utoipa::ToSchema;
 use crate::schemas::validators::{ApiKey, Page, Size};
-use crate::schemas::pagination::{Pagination, PaginationBuilder};
+use crate::schemas::pagination::Pagination;
+use crate::schemas::select_aud::SelectAuditoryOut;
 use crate::schemas::site_stat::SiteStatisticsOut;
 use crate::traits::Paginate;
 
@@ -34,14 +35,45 @@ impl Paginate<Pagination<SiteStatisticsOut>> for Filter {
         let total_items = pages.num_items().await?;
         let all_pages = pages.num_pages().await?;
         let items = pages.fetch_page(self.page.get()-1).await?;
-        let builder: PaginationBuilder<SiteStatisticsOut> = Pagination::builder();
-        Ok(builder
-            .items(items.into_iter().map(|model| model.into()).collect())
-            .total_items(total_items)
-            .all_pages(all_pages)
-            .size(self.size.get())
-            .page(self.page.get())
-            .build()
-        )
+        Ok(return_answer::<SiteStatisticsOut>(
+            items.into_iter().map(|model| model.into()).collect(),
+            self, total_items, all_pages
+        ))
     }
+}
+
+impl Paginate<Pagination<SelectAuditoryOut>> for Filter {
+    async fn pagination(&self, db: &DatabaseConnection) -> Result<Pagination<SelectAuditoryOut>, DbErr> {
+        let pages = if let Some(user_id) = self.user_id {
+            let user_id = entity::prelude::UserId::find_by_id(user_id).one(db).await?;
+            let user_id = user_id.unwrap();
+            user_id.find_related(entity::prelude::SelectAud).order_by_asc(entity::select_aud::Column::Id)
+                .paginate(db, self.size.get())
+        } else {
+            entity::select_aud::Entity::find().order_by_asc(entity::select_aud::Column::Id)
+                .paginate(db, self.size.get())
+        };
+        let total_items = pages.num_items().await?;
+        let all_pages = pages.num_pages().await?;
+        let items = pages.fetch_page(self.page.get()-1).await?;
+        Ok(return_answer::<SelectAuditoryOut>(
+            items.into_iter().map(|model| model.into()).collect(),
+            self, total_items, all_pages
+        ))
+    }
+}
+
+fn return_answer<T: Serialize + Clone + Default>(
+    items: Vec<T>,
+    data: &Filter,
+    total_items: u64,
+    all_pages: u64
+) -> Pagination<T> {
+    Pagination::builder()
+        .items(items.into_iter().map(|model| model.into()).collect())
+        .total_items(total_items)
+        .all_pages(all_pages)
+        .size(data.size.get())
+        .page(data.page.get())
+        .build()
 }
