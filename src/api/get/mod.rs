b86cdb5b36ca1cbee832879ac8dfb162147ae83c
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-08 22:31:37 +0300

    Add new endpoint handler

--- src/api/get/mod.rs
+++ src/api/get/mod.rs
@@ -1,4 +1,5 @@
 pub mod user_id;
+pub mod sites;
 
 use actix_web::web;
 
@@ -6,5 +7,6 @@ pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/get")
             .service(user_id::get_user_id)
+            .service(sites::get_sites)
     );
 }
