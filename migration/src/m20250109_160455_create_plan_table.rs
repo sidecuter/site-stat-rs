b137b2e2e7e7582495aca6ed5162423e183a1cfb
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-09 19:37:51 +0300

    Add new migration for ChangePlan table

--- migration/src/m20250109_160455_create_plan_table.rs
+++ migration/src/m20250109_160455_create_plan_table.rs
@@ -34,7 +34,7 @@ impl MigrationTrait for Migration {
 }
 
 #[derive(DeriveIden)]
-enum Plan {
+pub enum Plan {
     Table,
     Id,
 }
--- migration/src/m20250109_162921_create_change_plan_table.rs
+++ migration/src/m20250109_162921_create_change_plan_table.rs
@@ -1,6 +1,6 @@
 use sea_orm_migration::{prelude::*, schema::*};
 use crate::m20220101_000001_create_table::UserId;
-use crate::m20250108_120158_create_auds_table::Aud;
+use crate::m20250109_160455_create_plan_table::Plan;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -9,55 +9,50 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
+
         manager
             .create_table(
                 Table::create()
-                    .table(StartWay::Table)
+                    .table(ChangePlan::Table)
                     .if_not_exists()
-                    .col(pk_auto(StartWay::Id))
-                    .col(uuid(StartWay::UserId))
-                    .col(string(StartWay::StartId).not_null())
-                    .col(string(StartWay::EndId).not_null())
-                    .col(date_time(StartWay::VisitDate).not_null())
+                    .to_owned()
+                    .col(pk_auto(ChangePlan::Id))
+                    .col(uuid(ChangePlan::UserId))
+                    .col(date_time(ChangePlan::VisitDate))
+                    .col(string(ChangePlan::PlanId))
                     .foreign_key(
                         ForeignKey::create()
-                            .from(StartWay::Table, StartWay::UserId)
+                            .from(ChangePlan::Table, ChangePlan::UserId)
                             .to(UserId::Table, UserId::UserId)
                             .on_update(ForeignKeyAction::Cascade)
                             .on_delete(ForeignKeyAction::Cascade)
                     )
                     .foreign_key(
                         ForeignKey::create()
-                            .from(StartWay::Table, StartWay::StartId)
-                            .to(Aud::Table, Aud::Id)
-                            .on_update(ForeignKeyAction::Cascade)
+                            .from(ChangePlan::Table, ChangePlan::PlanId)
+                            .to(Plan::Table, Plan::Id)
                             .on_delete(ForeignKeyAction::Cascade)
-                    )
-                    .foreign_key(
-                        ForeignKey::create()
-                            .from(StartWay::Table, StartWay::EndId)
-                            .to(Aud::Table, Aud::Id)
                             .on_update(ForeignKeyAction::Cascade)
-                            .on_delete(ForeignKeyAction::Cascade)
                     )
                     .to_owned()
-            ).await
+            )
+            .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
+
         manager
-            .drop_table(Table::drop().table(StartWay::Table).to_owned())
+            .drop_table(Table::drop().table(ChangePlan::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum StartWay {
+enum ChangePlan {
     Table,
     Id,
     UserId,
-    StartId,
-    EndId,
-    VisitDate
+    VisitDate,
+    PlanId
 }
