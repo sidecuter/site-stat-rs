9f92d874cff812680ae0e2761b328d49ba052f2d
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-09 19:22:34 +0300

    Add new migration for table population

---
+++ migration/src/m20250109_160938_populate_plans_table.rs
@@ -0,0 +1,26 @@
+use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::sea_orm::EntityTrait;
+use entity::plan::ActiveModel;
+use entity::prelude::Plan;
+use crate::data::PLANS;
+use crate::sea_orm::ActiveValue::Set;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        let db = manager.get_connection();
+        let plan: Vec<_> = PLANS.iter().map(|plan| ActiveModel {id: Set(plan.to_string())}).collect();
+        Plan::insert_many(plan).exec(db).await.map(|_| ())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        let db = manager.get_connection();
+        Plan::delete_many().exec(db).await.map(|_| ())
+    }
+}
