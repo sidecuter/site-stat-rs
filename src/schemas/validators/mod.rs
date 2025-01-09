a31d2f9b87311e1dd6316d48dd88f9bdfb36692b
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-09 19:28:12 +0300

    Add new validator

--- src/schemas/validators/mod.rs
+++ src/schemas/validators/mod.rs
@@ -2,8 +2,10 @@ mod auditory;
 mod api_key;
 mod page;
 mod size;
+mod plan;
 
 pub use self::auditory::AuditoryId;
 pub use self::api_key::ApiKey;
 pub use self::page::Page;
 pub use self::size::Size;
+pub use self::plan::PlanId;
---
+++ src/schemas/validators/plan.rs
@@ -0,0 +1,41 @@
+use std::fmt::{Display, Formatter};
+use serde::{Deserialize, Deserializer, Serialize};
+use regex::Regex;
+use utoipa::ToSchema;
+
+#[derive(Debug, Serialize, Clone, ToSchema)]
+#[schema(example = "A-0", pattern = r"^([ABVN]D?-\d)$")]
+pub struct PlanId(
+    String
+);
+
+impl PlanId {
+    const REGEX: &'static str = r"^([ABVN]D?-\d)$";
+
+    fn validate(&self) -> Result<(), String> {
+        let re = Regex::new(Self::REGEX).unwrap();
+        re.is_match(&self.0).then(|| ()).ok_or(format!("Invalid plan_id format: {}", self.0))
+    }
+
+    pub fn new(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl<'de> Deserialize<'de> for PlanId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: String = Deserialize::deserialize(deserializer)?;
+        let plan_id = PlanId(s);
+        plan_id.validate().map_err(serde::de::Error::custom)?;
+        Ok(plan_id)
+    }
+}
+
+impl Display for PlanId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
