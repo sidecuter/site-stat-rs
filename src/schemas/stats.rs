418bba0d82fc7e89f96043a471661db823420ff9
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-17 20:44:43 +0300

    Change Statistics period datatype

--- src/schemas/stats.rs
+++ src/schemas/stats.rs
@@ -1,15 +1,15 @@
 use actix_web::body::BoxBody;
 use actix_web::Responder;
-use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
+use crate::schemas::Period;
 
 #[derive(Serialize, Deserialize, ToSchema, Debug, Clone)]
 pub struct Statistics {
     pub unique: usize,
     pub count: usize,
     pub all: usize,
-    pub period: Option<(NaiveDateTime, NaiveDateTime)>,
+    pub period: Period,
 }
 
 impl Responder for Statistics {
