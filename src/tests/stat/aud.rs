2ffda785d386d6159ebd93e047cbe582e516a2ea
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-01-20 16:47:38 +0300

    Fix tests for endpoint

--- src/tests/stat/aud.rs
+++ src/tests/stat/aud.rs
@@ -1,3 +1,4 @@
+use std::net::{IpAddr, SocketAddr};
 use super::super::prepare_connection;
 use crate::api::stat::aud::stat_aud;
 use crate::schemas::{SelectAuditoryIn, Status};
@@ -42,13 +43,40 @@ async fn stat_aud_endpoint(
     let req = test::TestRequest::put()
         .uri("/select-aud")
         .set_json(payload.clone())
+        .peer_addr(SocketAddr::new(IpAddr::from([192, 168, 1, 1]), 55050))
         .to_request();
     let resp = test::call_service(&app, req).await;
     assert_eq!(resp.status(), status_code);
     let req = test::TestRequest::put()
         .uri("/select-aud")
         .set_json(payload.clone())
+        .peer_addr(SocketAddr::new(IpAddr::from([192, 168, 1, 2]), 55050))
         .to_request();
     let resp: Status = test::call_and_read_body_json(&app, req).await;
     assert_eq!(resp, status);
+}
+
+#[rstest]
+#[tokio::test]
+async fn test_429_stat_aud_endpoint (
+    #[future(awt)] prepare_connection: Result<DatabaseConnection, Box<dyn std::error::Error>>,
+) {
+    assert!(prepare_connection.is_ok());
+    let db = prepare_connection.unwrap();
+    let app = test::init_service(App::new().app_data(Data::new(db)).service(stat_aud)).await;
+    let payload = SelectAuditoryIn {
+        user_id: uuid::Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1").unwrap(),
+        auditory_id: "a-100".into(),
+        success: true,
+    };
+    let mut status: u16 = 200;
+    for _ in 0..2 {
+        let req = test::TestRequest::put()
+            .uri("/select-aud")
+            .set_json(payload.clone())
+            .peer_addr(SocketAddr::new(IpAddr::from([192, 168, 1, 10]), 55050))
+            .to_request();
+        status = test::call_service(&app, req).await.status().into();
+    }
+    assert_eq!(status, 429);
 }
