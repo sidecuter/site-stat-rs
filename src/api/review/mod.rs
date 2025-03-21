4611ae99377c6b467d8414ee9cd4cd6c62ee9191
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-21 19:48:01 +0300

    New endpoint for file retrieving

--- src/api/review/mod.rs
+++ src/api/review/mod.rs
@@ -1,5 +1,6 @@
 pub mod add;
 pub mod get;
+pub mod image;
 
 use actix_web::web;
 
@@ -8,5 +9,6 @@ pub fn init_routes(cfg: &mut web::ServiceConfig) {
         web::scope("/review")
             .service(add::add_review)
             .service(get::get_reviews)
+            .service(image::get_file)
     );
 }
