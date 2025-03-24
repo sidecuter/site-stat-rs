d94930492fc152f0c76d7cddc6370fdda4dd91e4
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-24 23:59:58 +0300

    Fix lfi

--- src/api/review/mod.rs
+++ src/api/review/mod.rs
@@ -1,18 +1,14 @@
 pub mod add;
 pub mod get;
+pub mod image;
 
 use actix_web::web;
-use crate::app_state::AppState;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/review")
             .service(add::add_review)
             .service(get::get_reviews)
-            .service(
-                actix_files::Files::new(
-                    "/image",
-                    AppState::new().files_path.clone()
-                ))
+            .service(image::get_image)
     );
 }
