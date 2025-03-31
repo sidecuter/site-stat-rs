c73da524a8247887395bd7fe06fd2e34c51c6755
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-03-31 18:19:06 +0300

    Add tests for UUID parse error

--- src/tests/stat/site.rs
+++ src/tests/stat/site.rs
@@ -5,6 +5,7 @@ use actix_web::web::Data;
 use actix_web::{test, App};
 use rstest::*;
 use sea_orm::DatabaseConnection;
+use utoipa::gen::serde_json::json;
 
 #[rstest]
 #[case::insert_correct("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec", "OK", 200)]
@@ -35,4 +36,22 @@ async fn stat_site_endpoint(
         .to_request();
     let resp: Status = test::call_and_read_body_json(&app, req).await;
     assert_eq!(resp, status);
+}
+
+#[rstest]
+#[tokio::test]
+async fn test_400_stat_site_endpoint(
+    #[future(awt)] prepare_connection: Result<DatabaseConnection, Box<dyn std::error::Error>>,
+) {
+    assert!(prepare_connection.is_ok());
+    let db = prepare_connection.unwrap();
+    let app = test::init_service(App::new().app_data(Data::new(db)).service(stat_site)).await;
+    let req = test::TestRequest::put()
+        .uri("/site")
+        .set_json(json!({
+            "user_id": "11e1a4b8-7fa7-4501-9faa-541a5e0ff1e"
+        }))
+        .to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), 400);
 }
