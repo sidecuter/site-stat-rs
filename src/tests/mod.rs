aabae83ae795bbc6131401743843cd01f60bb0fa
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-04-02 12:10:09 +0300

    Create helpers module

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,74 +1,13 @@
 mod get;
 mod stat;
 mod review;
+mod helpers;
+mod swagger;
 
-use actix_web::{test, web, App};
-use crate::entity::{
-    change_plan::ActiveModel as ChangePlan, select_aud::ActiveModel as SelectAuditory,
-    site_stat::ActiveModel as SiteStat, start_way::ActiveModel as StartWay,
-    user_id::ActiveModel as UserId,
-    review::ActiveModel as Review
-};
 use migration::{Migrator, MigratorTrait};
-use rstest::{fixture, rstest};
-use sea_orm::ActiveValue::Set;
-use sea_orm::{ActiveModelTrait, Database, DatabaseConnection};
-use utoipa_swagger_ui::SwaggerUi;
-use uuid::Uuid;
-use crate::api_docs;
-
-async fn prepare_database(db: &DatabaseConnection) -> Result<(), Box<dyn std::error::Error>> {
-    UserId {
-        user_id: Set(Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec")?),
-        creation_date: Set(chrono::Utc::now().naive_utc()),
-    }
-        .insert(db)
-        .await?;
-    SiteStat {
-        user_id: Set(Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec")?),
-        visit_date: Set(chrono::Utc::now().naive_utc()),
-        endpoint: Set(None),
-        ..Default::default()
-    }
-        .insert(db)
-        .await?;
-    SelectAuditory {
-        user_id: Set(Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec")?),
-        visit_date: Set(chrono::Utc::now().naive_utc()),
-        auditory_id: Set("a-100".into()),
-        success: Set(true),
-        ..Default::default()
-    }
-        .insert(db)
-        .await?;
-    StartWay {
-        user_id: Set(Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec")?),
-        visit_date: Set(chrono::Utc::now().naive_utc()),
-        start_id: Set("a-100".into()),
-        end_id: Set("a-101".into()),
-        ..Default::default()
-    }
-        .insert(db)
-        .await?;
-    ChangePlan {
-        user_id: Set(Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec")?),
-        visit_date: Set(chrono::Utc::now().naive_utc()),
-        plan_id: Set("A-0".into()),
-        ..Default::default()
-    }
-        .insert(db)
-        .await?;
-    Review {
-        user_id: Set(Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec")?),
-        creation_date: Set(chrono::Utc::now().naive_utc()),
-        text: Set("Awesome review".to_owned()),
-        problem_id: Set("way".to_owned()),
-        ..Default::default()
-    }
-        .insert(db)
-        .await?;
-    Ok(())
-}
+use rstest::fixture;
+use sea_orm::{Database, DatabaseConnection};
+use self::helpers::prepare_database;
 
 #[fixture]
 async fn prepare_connection() -> Result<DatabaseConnection, Box<dyn std::error::Error>> {
@@ -77,24 +16,4 @@ async fn prepare_connection() -> Result<DatabaseConnection, Box<dyn std::error::
     Migrator::up(&pool, None).await?;
     prepare_database(&pool).await?;
     Ok(pool)
-}
-
-#[rstest]
-#[tokio::test]
-async fn test_swagger() {
-    let app = test::init_service(
-        App::new()
-            .service(
-                // OpenAPI document
-                web::scope("/docs").service(api_docs::openapi_json).service(
-                    SwaggerUi::new("/swagger/{_:.*}").url("/docs/openapi.json", Default::default()),
-                ),
-            )
-    ).await;
-    let req = test::TestRequest::get().uri("/docs/openapi.json").to_request();
-    let resp = test::call_service(&app, req).await;
-    assert_eq!(resp.status().as_u16(), 200);
-    let req = test::TestRequest::get().uri("/docs/swagger/").to_request();
-    let resp = test::call_service(&app, req).await;
-    assert_eq!(resp.status().as_u16(), 200)
 }
--- src/tests/review/add.rs
+++ src/tests/review/add.rs
@@ -2,104 +2,18 @@ use std::fs;
 use super::super::prepare_connection;
 use crate::api::review::add::add_review;
 use crate::schemas::{Status, Problem};
-use actix_web::web::{BufMut, Bytes, BytesMut, Data};
+use actix_web::web::{Bytes, Data};
 use actix_web::{test, App};
-use actix_web::http::header;
-use rand::{distr::{Alphanumeric, SampleString}, rng};
-use actix_web::http::header::HeaderMap;
-use mime::Mime;
 use rstest::*;
 use sea_orm::DatabaseConnection;
 use crate::app_state::AppState;
-
-const CRLF: &[u8] = b"\r\n";
-const CRLF_CRLF: &[u8] = b"\r\n\r\n";
-const HYPHENS: &[u8] = b"--";
-const BOUNDARY_PREFIX: &str = "------------------------";
-
-pub const BLACK_1X1_PNG: &[u8] = &[
-    0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A,
-    0x00, 0x00, 0x00, 0x0D,
-    0x49, 0x48, 0x44, 0x52,
-    0x00, 0x00, 0x00, 0x01,
-    0x00, 0x00, 0x00, 0x01,
-    0x08,
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-    0x8C, 0x69, 0x82, 0x8A,
-    0x00, 0x00, 0x00, 0x0C,
-    0x49, 0x44, 0x41, 0x54,
-    0x78, 0x01,
-    0x01, 0x02, 0x00, 0xFD,
-    0xFF, 0x00, 0x00, 0x01,
-    0x00, 0x01,
-    0x9A, 0x1C, 0x21, 0xBC,
-    0x00, 0x00, 0x00, 0x00,
-    0x49, 0x45, 0x4E, 0x44,
-    0xAE, 0x42, 0x60, 0x82,
-];
-
-pub fn generate_multipart_payload(
-    user_id: String,
-    problem: Problem,
-    text: String,
-    file: Option<(String, Bytes, Mime)>
-) -> (Bytes, HeaderMap) {
-    let boundary = Alphanumeric.sample_string(&mut rng(), 32);
-    let boundary_str = [BOUNDARY_PREFIX, &boundary].concat();
-    let boundary = boundary_str.as_bytes();
-    let sub = |buf: &mut BytesMut, name, val: String| {
-        buf.put(CRLF);
-        buf.put(format!("Content-Disposition: form-data; name=\"{name}\"").as_bytes());
-        buf.put(CRLF_CRLF);
-        buf.put(val.as_bytes());
-        buf.put(CRLF);
-        buf.put(HYPHENS);
-        buf.put(boundary);
-    };
-    let mut buf = if let Some((_, filebuf, _)) = file.clone() {
-        BytesMut::with_capacity(filebuf.len() + 128 + 330)
-    } else {
-        BytesMut::with_capacity(330)
-    };
-
-    buf.put(HYPHENS);
-    buf.put(boundary);
-    sub(&mut buf, "user_id", user_id);
-    sub(&mut buf, "problem", problem.to_string());
-    sub(&mut buf, "text", text);
-    if let Some((filename, filebuf, mimetype)) = file {
-        buf.put(CRLF);
-        buf.put("Content-Disposition: form-data; name=\"image\"".as_bytes());
-        buf.put(format!("; filename=\"{filename}\"",).as_bytes());
-        buf.put(CRLF);
-        buf.put(format!("Content-Type: {}", mimetype).as_bytes());
-        buf.put(CRLF_CRLF);
-
-        buf.put(filebuf);
-        buf.put(CRLF_CRLF);
-
-        buf.put(HYPHENS);
-        buf.put(boundary);
-    }
-    buf.put(HYPHENS);
-    buf.put(CRLF);
-
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        header::CONTENT_TYPE,
-        format!("multipart/form-data; boundary=\"{boundary_str}\"").parse().unwrap()
-    );
-    let bytes = buf.freeze();
-    (bytes, headers)
-}
+use super::super::helpers::{prepare_tmp_dir, BLACK_1X1_PNG, generate_multipart_payload};
 
 enum FileType {
     Image,
     Text,
-    None
+    None,
+    Invalid
 }
 
 #[rstest]
@@ -113,7 +27,7 @@ enum FileType {
 )]
 #[case::insert_incorrect_user(
     "11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1",
-    Problem::Way,
+    Problem::Work,
     "awesome text",
     FileType::None,
     "User not found",
@@ -121,7 +35,7 @@ enum FileType {
 )]
 #[case::wrong_file_type(
     "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
-    Problem::Way,
+    Problem::Plan,
     "awesome text",
     FileType::Text,
     "This endpoint accepts only images",
@@ -129,11 +43,19 @@ enum FileType {
 )]
 #[case::ok_with_file(
     "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
-    Problem::Way,
+    Problem::Other,
     "awesome text",
     FileType::Image,
     "OK",
     200
+)]
+#[case::invalid_mime(
+    "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
+    Problem::Other,
+    "awesome text",
+    FileType::Invalid,
+    "File has no mime type",
+    422
 )]
 #[tokio::test]
 async fn add_review_endpoint(
@@ -146,22 +68,24 @@ async fn add_review_endpoint(
     #[case] status_code: u16,
 ) {
     assert!(prepare_connection.is_ok());
-    let filepath = format!("/tmp/{}", uuid::Uuid::new_v4());
-    std::env::set_var("FILES_PATH", filepath.clone());
-    let appstate = AppState::new();
-    if !std::path::Path::new(&appstate.files_path).exists() {
-        fs::create_dir(appstate.files_path.clone()).unwrap();
-    }
+    let filepath = prepare_tmp_dir();
     let db = prepare_connection.unwrap();
-    let app = test::init_service(App::new().app_data(Data::new(db)).app_data(Data::new(appstate)).service(add_review)).await;
+    let app = test::init_service(App::new()
+        .app_data(Data::new(db))
+        .app_data(Data::new(AppState::default()))
+        .service(add_review)
+    ).await;
     let file = match file_type {
         FileType::Image => Some(
-            ("image.png".to_string(), Bytes::from_static(BLACK_1X1_PNG), mime::IMAGE_PNG)
+            ("image.png".to_string(), Bytes::from_static(BLACK_1X1_PNG), Some(mime::IMAGE_PNG))
         ),
         FileType::Text => Some(
-            ("test.txt".to_string(), Bytes::from_static(b"Lorem ipsum."), mime::TEXT_PLAIN)
+            ("test.txt".to_string(), Bytes::from_static(b"Lorem ipsum."), Some(mime::TEXT_PLAIN))
         ),
         FileType::None => None,
+        FileType::Invalid => Some(
+            ("test.txt".to_string(), Bytes::from_static(b"Lorem ipsum."), None)
+        )
     };
     let (payload, header) = generate_multipart_payload(user_id, problem, text, file);
     let req = test::TestRequest::post()
