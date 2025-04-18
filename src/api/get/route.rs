bb27b955659f0666388f1c2e9918070bae9f0326
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-04-19 00:59:28 +0300

    Add new endpoint

---
+++ src/api/get/route.rs
@@ -0,0 +1,75 @@
+use crate::errors::{ApiError, ApiResult};
+use crate::schemas::Status;
+use actix_web::{get, web};
+use crate::mut_state::AppStateMutable;
+use crate::schemas::data::get_graph;
+use crate::schemas::filter::FilterRoute;
+use crate::schemas::graph::ShortestWay;
+
+#[utoipa::path(
+    get,
+    path = "/api/get/route",
+    params(
+        ("from_p" = inline(String), Query, example = "a-102"),
+        ("to_p" = inline(String), Query, example = "a-109"),
+        ("loc" = inline(String), Query, example = "campus_BS"),
+    ),
+    responses(
+        (
+            status = 200, description = "Generated route from one point to another", body = ShortestWay,
+            example = json!(
+                {
+                    "way": [
+                        {
+                            "id": "a-102",
+                            "x": 890,
+                            "y": 1581,
+                            "type": "entrancesToAu"
+                        },
+                        {
+                            "id": "a-1_21",
+                            "x": 890,
+                            "y": 1530,
+                            "type": "hallway"
+                        },
+                        {
+                            "id": "a-1_22",
+                            "x": 884,
+                            "y": 1530,
+                            "type": "hallway"
+                        },
+                        {
+                            "id": "a-109",
+                            "x": 884,
+                            "y": 1480,
+                            "type": "entrancesToAu"
+                        }
+                    ],
+                    "distance": 107
+                }
+            )
+        ),
+        (
+            status = 404, description = "Point not found", body = Status,
+            example = json!({"status": "You are trying to get a route along non-existent vertex"})
+        ),
+        (
+            status = 500, description = "No such graph", body = Status,
+            example = json!(Status{status: "No graphs loaded".to_string()})
+        )
+    ),
+    tag = "Get"
+)]
+#[get("/route")]
+async fn get_route(
+    query: web::Query<FilterRoute>,
+    app_state: web::Data<AppStateMutable>
+) -> ApiResult<ShortestWay> {
+    let graph = get_graph(app_state.data_entry.lock()?, &query.loc.to_string())
+        .await
+        .ok_or(ApiError::InternalError("No graphs loaded".to_string()))?;
+    if !graph.has_vertex(&query.from_p) || !graph.has_vertex(&query.to_p) {
+        Err(ApiError::NotFound("You are trying to get a route along non-existent vertex".to_string()))?
+    }
+    Ok(graph.get_shortest_way_from_to(&query.from_p, &query.to_p))
+}
