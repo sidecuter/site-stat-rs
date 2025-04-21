d4da8dbc595ec28a2452e8cc3db0bc2d04bd4231
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-04-21 16:21:57 +0300

    Use mock db, not actual for stat/change-plan endpoint

--- src/tests/stat/plan.rs
+++ src/tests/stat/plan.rs
@@ -1,57 +1,100 @@
-use super::super::prepare_connection;
 use crate::api::stat::plan::stat_plan;
-use crate::schemas::{ChangePlanIn, Status};
+use crate::entity::{change_plan, plan, user_id};
+use crate::schemas::ChangePlanIn;
+use crate::tests::get_db;
 use actix_web::web::Data;
 use actix_web::{test, App};
 use rstest::*;
-use sea_orm::DatabaseConnection;
+use sea_orm::{DbBackend, MockDatabase, MockExecResult, MockRow};
+use std::str::FromStr;
 
 #[rstest]
-#[case::insert_correct("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec", "A-0", "OK", 200)]
-#[case::insert_incorrect_user("11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1", "A-0", "User not found", 404)]
-#[case::insert_incorrect_plan(
-    "11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec",
-    "A-8",
-    "Changed plan not found",
-    404
-)]
 #[tokio::test]
-async fn stat_plan_endpoint(
-    #[future(awt)] prepare_connection: Result<DatabaseConnection, Box<dyn std::error::Error>>,
-    #[case] user_id: String,
-    #[case] plan_id: String,
-    #[case] status: Status,
-    #[case] status_code: u16,
-) {
-    assert!(prepare_connection.is_ok());
-    let db = prepare_connection.unwrap();
-    let app = test::init_service(App::new().app_data(Data::new(db)).service(stat_plan)).await;
+async fn test_200_stat_plan() {
+    let db = Data::new(
+        MockDatabase::new(DbBackend::Sqlite)
+            .append_query_results([[user_id::Model {
+                user_id: uuid::Uuid::from_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec").unwrap(),
+                creation_date: chrono::Utc::now().naive_utc(),
+            }]])
+            .append_query_results([[plan::Model {
+                id: "A-0".to_string(),
+            }]])
+            .append_query_results([[change_plan::Model {
+                id: 0,
+                user_id: uuid::Uuid::from_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec").unwrap(),
+                visit_date: chrono::Utc::now().naive_utc(),
+                plan_id: "A-0".to_string(),
+            }]])
+            .append_exec_results([MockExecResult {
+                last_insert_id: 0,
+                rows_affected: 1,
+            }])
+            .into_connection(),
+    );
+    let app = test::init_service(App::new().app_data(db).service(stat_plan)).await;
     let payload = ChangePlanIn {
-        user_id: uuid::Uuid::parse_str(&user_id).unwrap(),
-        plan_id,
+        user_id: uuid::Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1ec").unwrap(),
+        plan_id: "A-0".to_string(),
     };
     let req = test::TestRequest::put()
         .uri("/change-plan")
         .set_json(payload.clone())
         .to_request();
     let resp = test::call_service(&app, req).await;
-    assert_eq!(resp.status(), status_code);
-    let req = test::TestRequest::put()
-        .uri("/change-plan")
-        .set_json(payload.clone())
-        .to_request();
-    let resp: Status = test::call_and_read_body_json(&app, req).await;
-    assert_eq!(resp, status);
+    assert_eq!(resp.status(), 200);
 }
 
 #[rstest]
 #[tokio::test]
-async fn test_429_stat_plan_endpoint(
-    #[future(awt)] prepare_connection: Result<DatabaseConnection, Box<dyn std::error::Error>>,
-) {
-    assert!(prepare_connection.is_ok());
-    let db = prepare_connection.unwrap();
-    let app = test::init_service(App::new().app_data(Data::new(db)).service(stat_plan)).await;
+async fn test_404_stat_plan_user() {
+    let db = Data::new(
+        MockDatabase::new(DbBackend::Sqlite)
+            .append_query_results::<MockRow, Vec<_>, Vec<Vec<_>>>(vec![vec![]])
+            .into_connection(),
+    );
+    let app = test::init_service(App::new().app_data(db).service(stat_plan)).await;
+    let payload = ChangePlanIn {
+        user_id: uuid::Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1").unwrap(),
+        plan_id: "A-0".to_string(),
+    };
+    let req = test::TestRequest::put()
+        .uri("/change-plan")
+        .set_json(payload.clone())
+        .to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), 404);
+}
+
+#[rstest]
+#[tokio::test]
+async fn test_404_stat_plan_plan() {
+    let db = Data::new(
+        MockDatabase::new(DbBackend::Sqlite)
+            .append_query_results([[user_id::Model {
+                user_id: uuid::Uuid::from_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1").unwrap(),
+                creation_date: chrono::Utc::now().naive_utc(),
+            }]])
+            .append_query_results::<MockRow, Vec<_>, Vec<Vec<_>>>(vec![vec![]])
+            .into_connection(),
+    );
+    let app = test::init_service(App::new().app_data(db).service(stat_plan)).await;
+    let payload = ChangePlanIn {
+        user_id: uuid::Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1").unwrap(),
+        plan_id: "A-8".to_string(),
+    };
+    let req = test::TestRequest::put()
+        .uri("/change-plan")
+        .set_json(payload.clone())
+        .to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), 404);
+}
+
+#[rstest]
+#[tokio::test]
+async fn test_429_stat_plan_endpoint() {
+    let app = test::init_service(App::new().app_data(get_db()).service(stat_plan)).await;
     let payload = ChangePlanIn {
         user_id: uuid::Uuid::parse_str("11e1a4b8-7fa7-4501-9faa-541a5e0ff1e1").unwrap(),
         plan_id: "A-".into(),
