e4e7368f830d9fce3cb8913a13bf94143e35c376
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-04-21 16:07:06 +0300

    Reformat user_id test

--- src/tests/get/user.rs
+++ src/tests/get/user.rs
@@ -1,29 +1,25 @@
 use crate::api::get::user_id::get_user_id;
+use crate::entity::user_id;
 use actix_web::web::Data;
 use actix_web::{test, App};
 use rstest::*;
 use sea_orm::{DbBackend, MockDatabase, MockExecResult};
-use crate::entity::user_id;
 
 #[rstest]
 #[tokio::test]
 async fn get_user_endpoint() {
-    let db = Data::new(MockDatabase::new(DbBackend::Sqlite)
-        .append_query_results([
-            vec![
-                user_id::Model {
-                    user_id: uuid::Uuid::new_v4(),
-                    creation_date: chrono::Utc::now().naive_utc()
-                }
-            ]
-        ])
-        .append_exec_results([
-            MockExecResult {
+    let db = Data::new(
+        MockDatabase::new(DbBackend::Sqlite)
+            .append_query_results([vec![user_id::Model {
+                user_id: uuid::Uuid::new_v4(),
+                creation_date: chrono::Utc::now().naive_utc(),
+            }]])
+            .append_exec_results([MockExecResult {
                 last_insert_id: 0,
-                rows_affected: 1
-            }
-        ])
-        .into_connection());
+                rows_affected: 1,
+            }])
+            .into_connection(),
+    );
     let app = test::init_service(App::new().app_data(db).service(get_user_id)).await;
     let req = test::TestRequest::get().uri("/user-id").to_request();
     let resp = test::call_service(&app, req).await;
