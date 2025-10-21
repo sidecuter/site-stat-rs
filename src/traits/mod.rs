f907f45032517a5b02b0a0dd7675c2a1bac1719d
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2025-10-22 01:30:57 +0300

    Add trait for entity ids generation

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,7 +1,9 @@
 mod convert;
 mod filter;
 mod paginate;
+mod entity_id;
 
 pub use self::convert::{ConversionToStatusTrait, ConversionTrait};
 pub use self::filter::FilterTrait;
 pub use self::paginate::Paginate;
+pub use self::entity_id::EntityId;
